a562b26dca2d82202fe8ae2c9865f46f19f05555
ursulafahle <161364127+ursulafahle@users.noreply.github.com>
2024-05-03 18:16:35 +0200

    changes for having an automatic forecast

--- src/index.js
+++ src/index.js
@@ -47,7 +47,11 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
-
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  return days[date.getDate()];
+}
 function getForecast(city) {
   let apiKey = "14600o81bf492c92cb64c3ab43ad1f9t";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -56,23 +60,30 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
   let forecastHtml = "";
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
         <div class="weather-forecast">
           <div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">☀️</div>
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+            <div > <img src="${
+              day.condition.icon_url
+            }"class="weather-forecast-icon"/>
             <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-max">15° </span>
-              <span class="weather-forecast-min">12° </span>
+              
+            <span class="weather-forecast-max">${Math.round(
+              day.temperature.maximum
+            )}°</span>
+              <span class="weather-forecast-min">${Math.round(
+                day.temperature.minimum
+              )}°</span>
             </div>
           </div>
         </div>`;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
